7ba4f38d916b2ec2ec91c702650433b37ee6f633
sir Random <random@amberskynet.org>
2025-11-16 10:00:08 +0300

    updates

--- core/src/modules/exporter.rs
+++ core/src/modules/exporter.rs
@@ -1,4 +1,4 @@
-use crate::modules::components::{Pos, Rot};
+use crate::modules::components::{MaxSpeed, Pos, Rot, ToxicPower};
 use crate::modules::entities::Vehicle;
 use crate::modules::entities::Waste;
 use crate::modules::state::State;
@@ -19,19 +19,21 @@ pub fn export_to_json(world: &World, state: &State) -> String {
     let mut vehicles = Vec::new();
 
     // Выборка всех waste
-    for (_id, (pos, _waste)) in world.query::<(&Pos, &Waste)>().iter() {
+    for (_id, (pos, toxic_power, _waste)) in world.query::<(&Pos, &ToxicPower, &Waste)>().iter() {
         wastes.push(HashMap::from([
             ("id".to_string(), Value::Number(_id.id().into())),
             ("pos".to_string(), serde_json::to_value(*pos).unwrap()),
+            ("toxic_power".to_string(), serde_json::to_value(*toxic_power).unwrap()),
         ]));
     }
 
     // Выборка всех vehicle
-    for (_id, (pos, rot, _vehicle)) in world.query::<(&Pos, &Rot, &Vehicle)>().iter() {
+    for (_id, (pos, rot, max_speed, _vehicle)) in world.query::<(&Pos, &Rot, &MaxSpeed, &Vehicle)>().iter() {
         vehicles.push(HashMap::from([
             ("id".to_string(), Value::Number(_id.id().into())),
             ("pos".to_string(), serde_json::to_value(*pos).unwrap()),
             ("rot".to_string(), serde_json::to_value(*rot).unwrap()),
+            ("max_speed".to_string(), serde_json::to_value(*max_speed).unwrap()),
         ]));
     }
 
