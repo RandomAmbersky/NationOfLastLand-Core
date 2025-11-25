ececad29334fb130548474304b9a4d97988a2543
mrstreamer963 <698lo7epy@mozmail.com>
2025-11-25 09:59:14 +0300

    update

--- core/src/modules/exporter.rs
+++ core/src/modules/exporter.rs
@@ -17,32 +17,32 @@ pub fn export_to_json(world: &World, state: &State) -> String {
 
     // Выборка всех alerts
     for (_id, (pos, unit_type )) in world.query::<(&Pos, &AlertType)>().iter() {
-        let mut alert_data = HashMap::from([
+        let alert_data = HashMap::from([
             ("id".to_string(), Value::Number(_id.id().into())),
             ("pos".to_string(), serde_json::to_value(*pos).unwrap()),
             ("unit_type".to_string(), serde_json::to_value(*unit_type).unwrap()),
         ]);
 
-        if let Ok(health) = world.get::<&Health>(_id) {
-            alert_data.insert("velocity".to_string(), serde_json::to_value(*health).unwrap());
-        }
+        // if let Ok(health) = world.get::<&Health>(_id) {
+        //     alert_data.insert("velocity".to_string(), serde_json::to_value(*health).unwrap());
+        // }
 
-        if let Ok(velocity) = world.get::<&Velocity>(_id) {
-            alert_data.insert("velocity".to_string(), serde_json::to_value(*velocity).unwrap());
-        }
+        // if let Ok(velocity) = world.get::<&Velocity>(_id) {
+        //     alert_data.insert("velocity".to_string(), serde_json::to_value(*velocity).unwrap());
+        // }
 
-        if let Ok(rot) = world.get::<&Velocity>(_id) {
-            alert_data.insert("rot".to_string(), serde_json::to_value(*rot).unwrap());
-        }
+        // if let Ok(rot) = world.get::<&Velocity>(_id) {
+        //     alert_data.insert("rot".to_string(), serde_json::to_value(*rot).unwrap());
+        // }
 
-        if let Ok(max_speed) = world.get::<&Velocity>(_id) {
-            alert_data.insert("max_speed".to_string(), serde_json::to_value(*max_speed).unwrap());
-        }
+        // if let Ok(max_speed) = world.get::<&Velocity>(_id) {
+        //     alert_data.insert("max_speed".to_string(), serde_json::to_value(*max_speed).unwrap());
+        // }
         
-        // Add optional components
-        if let Ok(target) = world.get::<&TargetPos>(_id) {
-            alert_data.insert("target".to_string(), serde_json::to_value(*target).unwrap());
-        }
+        // // Add optional components
+        // if let Ok(target) = world.get::<&TargetPos>(_id) {
+        //     alert_data.insert("target".to_string(), serde_json::to_value(*target).unwrap());
+        // }
 
         units.push(alert_data);
     }
