398244dbabad656df047b908fbed4a83d9d7eb72
mrstreamer963 <698lo7epy@mozmail.com>
2025-11-27 21:32:59 +0300

    update

--- core/src/modules/components/target_id.rs
+++ core/src/modules/components/target_id.rs
@@ -3,4 +3,4 @@ use serde::{Serialize, Deserialize};
 use crate::modules::components::Guid;
 
 #[derive(Serialize, Deserialize, Clone, Copy, Default)]
-pub struct TargetId(pub Guid);
+pub struct TargetId(pub Guid); // for serialize/deserialize only
